2d83df62449a32308e7c3c017eb3cf717fd21f58
DestinyF <destinyfawley@gmail.com>
2019-11-13 20:18:17 -0600

    Add files via upload

--- navigation/navInit.m
+++ navigation/navInit.m
@@ -1,6 +1,15 @@
-function nav = navInit(models)
+% Destiny Fawley
+% 11/6/2019
+
+function nav = navInit(models, y)
 
 
-nav = [];
+nav.rateRatio = round(models.integrationRate/models.navigationRate); % sim/nav rate ratio, nd
+
+% initialize states
+nav.posI = y(1:3);
+nav.velI = y(4:6);
+nav.EulerAngles = y(7:9);
+nav.omega = y(10:12);
 
 end
--- navigation/navigation.m
+++ navigation/navigation.m
@@ -1,11 +1,23 @@
-function nav = navigation(y,tCurr, trajCalcs, models)
+% Destiny Fawley
+% 11/6/2019
 
-switch models.navMode
-    case 1 % perfect nav
-        nav.posI = y(1:3);
-        nav.velI = y(4:6);
-        nav.EulerAngles = y(7:9);
-        nav.omega = y(10:12);
-        
+function nav = navigation(y,tCurr, trajCalcs, models, nav, i)
+
+
+if mod(i-1,nav.rateRatio) == 0 % rate limit calls to navigation
+    switch models.navMode
+        case 1 % perfect nav
+            nav.posI = y(1:3);
+            nav.velI = y(4:6);
+            nav.EulerAngles = y(7:9);
+            nav.omega = y(10:12);
+            
+    end
 end
+
 end
+
+
+
+
+
